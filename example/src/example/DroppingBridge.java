0d7253e977553394acef87f099c00b42305d8663
gelix-kyoungtae <winneris@gelix.co.kr>
2021-04-18 15:14:29 +0900

    1. BaekJoon 1010 solved!

---
+++ example/src/example/DroppingBridge.java
@@ -0,0 +1,55 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * 1010번 다리 놓기 
+ * 재원이는 한 도시의 시장이 되었다. 
+ * 이 도시에는 도시를 동쪽과 서쪽으로 나누는 큰 일직선 모양의 강이 흐르고 있다. 
+ * 하지만 재원이는 다리가 없어서 시민들이 강을 건너는데 큰 불편을 겪고 있음을 알고 다리를 짓기로 결심하였다. 
+ * 강 주변에서 다리를 짓기에 적합한 곳을 사이트라고 한다. 
+ * 재원이는 강 주변을 면밀히 조사해 본 결과 강의 서쪽에는 N개의 사이트가 있고 동쪽에는 M개의 사이트가 있다는 것을 알았다. (N ≤ M)
+ * 재원이는 서쪽의 사이트와 동쪽의 사이트를 다리로 연결하려고 한다. (이때 한 사이트에는 최대 한 개의 다리만 연결될 수 있다.) 
+ * 재원이는 다리를 최대한 많이 지으려고 하기 때문에 서쪽의 사이트 개수만큼 (N개) 다리를 지으려고 한다. 
+ * 다리끼리는 서로 겹쳐질 수 없다고 할 때 다리를 지을 수 있는 경우의 수를 구하는 프로그램을 작성하라.
+ * 
+ * 
+ * 조합의 공식으로 계산 할 수 있음. mCn
+ * 
+ * mCn = m-1Cn-1 + m-1Cn
+ * 
+ * @author kyoungtaekim
+ *
+ */
+
+public class DroppingBridge {
+	static int[][] db = null;
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int cnt = Integer.parseInt(br.readLine());
+			for(int i = 0 ; i < cnt ; i++) {
+				String str = br.readLine();
+				String[] values = str.split(" ");
+				int n = Integer.parseInt(values[0]);
+				int m = Integer.parseInt(values[1]);
+				db = new int[m+1][n+1];
+				System.out.println(solution(m, n));
+				db = null;
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static int solution(int m, int n) {
+		if(m == n || n == 0)
+			return 1;
+		if (db[m][n]>0)return db[m][n];
+		return db[m][n] = solution(m-1, n-1) + solution(m-1, n);
+	}
+}
