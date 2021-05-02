4aaa0fbf6024d71e94315bf4764fa7f9f080e8b7
winneris1234 <winneris1234@gmail.com>
2021-05-02 23:44:26 +0900

    BAEK JOON Question Solved! 9095
    - 문제
    정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
    
    1+1+1+1
    1+1+2
    1+2+1
    2+1+1
    2+2
    1+3
    3+1
    정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
    
    - 입력
    첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 n이 주어진다. n은 양수이며
    11보다 작다.
    
    - 출력
    각 테스트 케이스마다, n을 1, 2, 3의 합으로 나타내는 방법의 수를 출력한다.

---
+++ example/src/example/SumOneTwoThree9095.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class SumOneTwoThree9095 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			int cnt = Integer.parseInt(br.readLine());
+			for(int i = 0 ; i < cnt ; i++) {
+				int N = Integer.parseInt(br.readLine());
+				int	S = SUM(N);
+				bw.write(""+S);
+				bw.newLine();
+			}
+			bw.flush();
+		}catch(IOException e) {
+		}
+	}
+
+	public static int SUM(int root) {
+		if(root == 0)
+			return 1;
+		if(root < 0)
+			return 0;
+		return SUM(root-1)+SUM(root-2)+SUM(root-3);
+	}
+}
