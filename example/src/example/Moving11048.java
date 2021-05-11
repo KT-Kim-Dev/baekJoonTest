b077e84f6a5c6aea965398d4b36741b5a7c897d2
KT-Kim-Dev <68525365+KT-Kim-Dev@users.noreply.github.com>
2021-05-11 13:07:14 +0900

    Update Moving11048.java
    백준 져지 문제 - 이동하기 1048 (Silver 1)
    
    해결쓰

--- example/src/example/Moving11048.java
+++ example/src/example/Moving11048.java
@@ -6,74 +6,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-/**
- * 
- * 준규는 N×M 크기의 미로에 갇혀있다. 미로는 1×1크기의 방으로 나누어져 있고, 각 방에는 사탕이 놓여져 있다. 
- * 미로의 가장 왼쪽 윗 방은 (1, 1)이고, 가장 오른쪽 아랫 방은 (N, M)이다.
- * 준규는 현재 (1, 1)에 있고, (N, M)으로 이동하려고 한다. 
- * 준규가 (r, c)에 있으면, (r+1, c), (r, c+1), (r+1, c+1)로 이동할 수 있고, 각 방을 방문할 때마다 방에 놓여져있는 사탕을 모두 가져갈 수 있다. 
- * 또, 미로 밖으로 나갈 수는 없다.
- * 준규가 (N, M)으로 이동할 때, 가져올 수 있는 사탕 개수의 최댓값을 구하시오.
- * 
- * @author kyoungtaekim
- *
- */
-
 public class Moving11048 {
-	public static int[][] MAP = null;
+	
 	public static int[][] MATRIX = null;
+	public static int[][] NM = null;
 	
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		try {
+			String mat = br.readLine();
+			String[] XY = mat.split(" ");
 			
-			String line = br.readLine();
-			String[] NM = line.split(" ");
+			int X = Integer.parseInt(XY[0]);
+			int Y = Integer.parseInt(XY[1]);
 			
-			int N = Integer.parseInt(NM[0]);
-			int M = Integer.parseInt(NM[1]);
+			NM = new int[X][Y];
+			MATRIX = new int[X][Y];
 			
-			MATRIX = new int[N][M];
-			// Initialized
-			for(int i = 0 ; i < N ; i++) {
-				String A = br.readLine();
-				String[] B = A.split(" ");
-				for(int j = 0 ; j < M ; j++) {
-					MATRIX[i][j] = Integer.parseInt(B[j]);
+			for(int i = 0 ; i < X ; i++) {
+				String line = br.readLine();
+				String[] ARR = line.split(" ");
+				for(int j = 0 ; j < Y ; j++) {
+					NM[i][j] = Integer.parseInt(ARR[j]);
 				}
 			}
-
-			// Memorization
-			getMaxCandies(0, 0, N, M);
 			
-			bw.write("" + MAP[N-1][M-1]);
+			MATRIX[0][0] = NM[0][0];
+			
+			// Memorizing
+			maxCandies(X, Y);
+			
+			bw.write("" + MATRIX[X-1][Y-1]);
 			bw.flush();
-		}catch(IOException e) {
-			
-		}
-		
+		}catch(IOException e) { }
 	}
 
-	public static int getMaxCandies(int X, int Y, int N, int M) {
-		int answer = 0;
-		MAP = new int[N][M];
+	public static void maxCandies(int N, int M) {
 		
-		for(  ; X < N ; X++) {
-			for( ; Y < M ; Y++) {
-				if((X<N-1) && MAP[X+1][Y] < MATRIX[X][Y]+MATRIX[X+1][Y])
-					MAP[X+1][Y] = MATRIX[X][Y]+MATRIX[X+1][Y];
-				
-				if((Y<M-1) && (MAP[X+1][Y+1] < MATRIX[X][Y]+MATRIX[X+1][Y+1]))
-					MAP[X+1][Y+1] = MATRIX[X][Y]+MATRIX[X+1][Y+1];
-				
-				if((X<N-1) && (Y<M-1) && MAP[X][Y+1] < MATRIX[X][Y]+MATRIX[X][Y+1])
-					MAP[X][Y+1] = MATRIX[X][Y]+MATRIX[X][Y+1];
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0; j < M ; j++) {
+				if((j>0) && MATRIX[i][j] < (NM[i][j] + MATRIX[i][j-1])) {
+					MATRIX[i][j] = NM[i][j] + MATRIX[i][j-1];
+				}
+				if((i>0) && (j>0) && ( MATRIX[i][j] < (NM[i][j] + MATRIX[i-1][j-1]) )) {
+					MATRIX[i][j] = NM[i][j] + MATRIX[i-1][j-1];
+				}
+				if((i>0) && (MATRIX[i][j] < (NM[i][j] + MATRIX[i-1][j]))) {
+					MATRIX[i][j] = NM[i][j] + MATRIX[i-1][j];
+				}
 			}
 		}
 		
-		return answer;
 	}
 	
 }
