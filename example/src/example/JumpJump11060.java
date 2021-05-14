febd71ab02e1b26f90194267b16dabeaa73531ed
Kim Kyoungtae <winneris1234@gmail.com>
2021-05-14 19:13:52 +0900

    백준 저지 문제 풀이 (점프점프 11060)
    50퍼 정도에서 틀렸습니다 뜸

--- example/src/example/JumpJump11060.java
+++ example/src/example/JumpJump11060.java
@@ -21,14 +21,35 @@ public class JumpJump11060 {
 			String An = br.readLine();
 			String[] As = An.split(" ");
 			
+			A = new int[N];
+			stepA = new int[N];
+		
 			for(int i = 0 ; i < N ; i++) {
 				A[i] = Integer.parseInt(As[i]);
+				stepA[i] = 1111;
 			}
 			
-			
-			
+			stepA[N-1] = 0;
+			for(int i = N-2 ; i >= 0 ; i--) {
+				int step = A[i];
+				for(int j = step ; j >= 0  ; j--) {
+					if((j+i) > N-1) {
+						stepA[i] = 1+stepA[N-1];
+						j=-1;
+						continue;
+					}
+					else {
+						if(stepA[i] > 1+stepA[j+i])
+							stepA[i] = 1+stepA[j+i];
+					}
+				}
+			}
+			int answer = stepA[0]>0?stepA[0]:-1;
+			bw.write("" + answer);
+			bw.flush();
 		}catch(IOException e) {}
 		
 	}
+	
 
 }
