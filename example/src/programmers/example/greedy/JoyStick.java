7076a55d30a13501ac8f860ce1b67bd3820d4eaa
Kim Kyoungtae <winneris1234@gmail.com>
2021-05-26 13:54:36 +0900

    프로그래머스 문제 미해결 - 90점

--- example/src/programmers/example/greedy/JoyStick.java
+++ example/src/programmers/example/greedy/JoyStick.java
@@ -4,27 +4,46 @@ public class JoyStick {
 
 	public static void main(String[] args) {
 
-		String name = "JAN";
+		String name = "ZAAAZAAAAAA";
 		int answer = solution(name);
 		
 		System.out.println(answer);
 	}
-	
+	//							  A B C D E F G H I J K   L  M  N  O  P  Q R S T U V W X Y Z
 	private static int[] ARRAY = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
 	
 	public static int solution(String name) {
 		int answer = 0;
 		char[] ch = name.toCharArray();
 		
-		answer = countStep(ch, 0);
+		char[] cha = new char[ch.length];
+		
+		for(int i = 0 ; i < cha.length ; i++)
+			cha[i] = 'A';
+		
+		answer = countStep(cha,ch, 0);
 		
 		return answer;
 	}
 	
-	public static int countStep(char[] ch , int position) {
+	public static boolean compare(char[] A, char[] B) {
+		if(A.length != B.length)
+			return false;
+		
+		for(int i = 0 ; i < A.length ; i++) {
+			if(A[i] != B[i])
+				return false;
+		}
+		return true;
+	}
+	
+	public static int countStep(char[] STR, char[] ch , int position) {
 		int answer = 0;
 		
-		if(position > 0 && position - ch.length == 0)
+		if( compare(STR,ch) )
+			return 0;
+		
+		if(position > 0 && position >= ch.length )
 			return 0;
 
 		if(position < 0 && position + ch.length == 0)
@@ -41,12 +60,16 @@ public class JoyStick {
 			int A = (int) (ch[ch.length+position]-'A');
 			int B = ARRAY[A];
 			
-			return 1+B+countStep(ch, position-1);
+			STR[ch.length+position] = ch[ch.length+position];
+			
+			return 1+B+countStep(STR, ch, position-1);
 		}else {
 			int A = (int) (ch[position]-'A');
 			int B = ARRAY[A];
 			
-			return 1+B+countStep(ch, position+1);
+			STR[position] = ch[position];
+			
+			return 1+B+countStep(STR, ch, position+1);
 		}
 		
 		
