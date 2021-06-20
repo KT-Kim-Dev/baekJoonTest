c18b4f3abc53809dc5437fc5a0b4eccc3b0bd504
winneris1234 <winneris1234@gmail.com>
2021-06-20 23:42:29 +0900

    프로그래머스 문제 해결 - [카카오인턴] 키패드 누르기
    손가락의 현재 위치에서 다음 버튼의 이동횟수 계산하는게 주요 포인트

--- example/src/programmers/example/kakaointern/PressedKeyPad.java
+++ example/src/programmers/example/kakaointern/PressedKeyPad.java
@@ -4,67 +4,90 @@ public class PressedKeyPad {
 
 	public static void main(String[] args) {
 
-		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+		int[] numbers = { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 };
 		String hand = "right";
-		
+
 		String answer = solution(numbers, hand);
-		System.out.println( answer );
-		
+		System.out.println(answer);
+
 	}
 
-	public static class KeyPad {
-		public int[][] num = {{3,1}, {0,0}, {0,1}, {0,2}, {1,0}, {1,1}, {1,2}, {2,0}, {2,1}, {2,2} };
-		public int[][] pad = {{1,2,3},{4,5,6},{7,8,9},{'*',0,'#'}};
-		public int positionX;
-		public int positionY;
-		public boolean hand = false;
-		
-		public KeyPad(boolean hand) {
-			if(hand) {
-				positionX = 3;
-				positionY = 0;
-			}else {
-				positionX = 3;
-				positionY = 2;
-			}
-			System.out.println(pad[positionX][positionY]);
-		}
-		
-		public int getNumPad() {
-			return pad[positionX][positionY];
-		}
-		
-		public int getMovingCnt(int pos) {
-			int result = 0;
-			
-			if(hand) {
-				if(pos == 1 || pos == 4 || pos == 7)
-					return 0;
-				
-				
-				
-			}else {
-				if(pos == 3 || pos == 6 || pos == 9)
-					return 0;
-				
-				
-			}
-			
-			return result;
-		}
-		
-		public void setPosition(int position) {
-			int[] a = num[position];
-			positionX = a[0];
-			positionY = a[1];
-		}
-	}
-	
+	public static int[][] num = { { 3, 1 }, { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 0 },
+			{ 2, 1 }, { 2, 2 } };
+	public static int[][] pad = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { '*', 0, '#' } };
+
 	public static String solution(int[] numbers, String hand) {
 		String answer = "";
-		
-		
+
+		String h = hand.equals("right") ? "R" : "L";
+
+		int leftThumb = '*';
+		int rightThumb = '#';
+
+		for (int i = 0; i < numbers.length; i++) {
+
+			int a = numbers[i];
+
+			if (a == 1 || a == 4 || a == 7 || a == '*') {
+				answer += "L";
+				leftThumb = a;
+			} else if (a == 3 || a == 6 || a == 9 || a == '#') {
+				answer += "R";
+				rightThumb = a;
+			} else {
+				int l = getCount(leftThumb, a);
+				int r = getCount(rightThumb, a);
+
+				if (l > r) {
+					answer += "R";
+					rightThumb = a;
+				} else if (r > l) {
+					answer += "L";
+					leftThumb = a;
+				} else {
+					answer += h;
+					if (h.equals("R"))
+						rightThumb = a;
+					else
+						leftThumb = a;
+				}
+			}
+
+		}
+
 		return answer;
+	}
+
+	public static int getCount(int thumbPosition, int key) {
+		int result = 0;
+		int[] position = { 0, 0 };
+		int[] keyP = num[key];
+
+		if (thumbPosition == '*') {
+			position[0] = 3;
+			position[1] = 0;
+		} else if (thumbPosition == '#') {
+			position[0] = 3;
+			position[1] = 2;
+		} else {
+			position = num[thumbPosition];
+		}
+
+		int a = position[0];
+		int b = position[1];
+		int c = keyP[0];
+		int d = keyP[1];
+		
+		int e = a>c?a-c:c-a;
+		int f = b>d?b-d:d-b;
+		
+		if(e != 0)
+			e = e*e/e;
+		if(f != 0)
+			f = f*f/f;
+		result = e+f;
+
+		return result;
 	}
 
 }
