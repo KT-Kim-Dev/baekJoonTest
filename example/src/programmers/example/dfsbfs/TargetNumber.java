55545f53adeeaa1e9f425bb149203c5eb38e2f6f
winneris1234 <winneris1234@gmail.com>
2021-06-23 23:50:26 +0900

    프로그래머스 문제 해결 - 타겟넘버
    제귀함수를 호출하여 DFS 탐색

--- example/src/programmers/example/dfsbfs/TargetNumber.java
+++ example/src/programmers/example/dfsbfs/TargetNumber.java
@@ -1,49 +1,39 @@
 package programmers.example.dfsbfs;
 
-import java.util.Arrays;
-
 public class TargetNumber {
 
 	public static void main(String[] args) {
 
-		int[] numbers = {1,1,1,1,1};
+		int[] numbers = { 1, 1, 1, 1, 1 };
 		int target = 3;
-		
+
 		int answer = solution(numbers, target);
-		
+
 		System.out.println(answer);
-		
+
 	}
-	
+
 	public static int solution(int[] numbers, int target) {
-        int answer = 0;
+		int answer = 0;
 
-        Arrays.sort(numbers);
+		answer = getSame(numbers, 0, 0, target);
 
-        
-        
-        return answer;
-    }
-	
-	
+		return answer;
+	}
+
 	public static int getSame(int[] numbers, int position, int sum, int target) {
 		int result = 0;
-		
-		if(position == numbers.length) {
-			return 0;
-		}else {
-			
-			int a = sum + numbers[position];
-			if(target == a) {
+
+		if (position == numbers.length) {
+			if (sum == target)
 				return 1;
-			}else {
-				
-			}
-			
+			else
+				return 0;
 		}
-		
-		
-		return result;
+
+		return getSame(numbers, position + 1, sum + numbers[position], target)
+				+ getSame(numbers, position + 1, sum - numbers[position], target);
+
 	}
 
 }
