c45e339a891a9bd89aebbb6849958e96c17dad21
winneris1234 <winneris1234@gmail.com>
2021-06-27 23:19:22 +0900

    프로그래머스 문제 - 아직 안품 - 실패율

---
+++ example/src/programmers/example/kakao/FailureRating.java
@@ -0,0 +1,23 @@
+package programmers.example.kakao;
+
+public class FailureRating {
+
+	public static void main(String[] args) {
+		int N = 5;
+		int[] stages = {2,1,2,6,2,4,3,3};
+		
+		int[] answer = solution(N, stages);
+		
+		for(int a : answer) {
+			System.out.print(" "+a);
+		}
+	}
+
+	public static int[] solution(int N, int[] stages) {
+		int[] answer = new int[N];
+		
+		
+		
+		return answer;
+	}
+}
