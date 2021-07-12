3ef7b6f8a65b6d7488d34751195a82aa6b60eb9f
winneris1234 <winneris1234@gmail.com>
2021-07-13 00:10:43 +0900

    프로그래머스 문제 풀이 - 짝지어 제거하기
    2017팁스타

--- example/src/programmers/example/tipstown/RemoveToCouple.java
+++ example/src/programmers/example/tipstown/RemoveToCouple.java
@@ -1,55 +1,57 @@
 package programmers.example.tipstown;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class RemoveToCouple {
 	
 	public static void main(String[] args) {
-		String s = "bbvvbbvvqbbvvbbqvvbbvvbbqvvbbvvqbbvvbbvv";
+		String s = "baabaa";
 		
 		int answer = solution(s);
 		
 		System.out.println(answer);
 	}
 	
-	public static int solution(String s)
-    {
-        int answer = 0;
+	public static String[] couples = {"aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp",
+			"qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"};
+	
+	public static int solution(String s) {
+		if(s.length() % 2 == 1)
+			return 0;
+		
+		Stack<String> stack = new Stack<String>();
+		
+		String[] strs = s.split("");
+		
+		int i = 0;
+		
+		while(true) {
+			if(i >= s.length())
+				break;
+			
+			if(stack.size() == 0){
+				stack.push(strs[i]);
+				i++;
+			}
+			
+			String str = stack.pop();
+			
+			if(str.equals(strs[i])) {
+				i++;
+			}else {
+				stack.push(str);
+				stack.push(strs[i]);
+				i++;
+			}
+		}
+		
+		if(stack.size() == 0)
+			return 1;
+		else
+			return 0;
+		
+	}
 
-        boolean isChanged = true;
-        List<String> list = new ArrayList<String>();
-        
-        for(int i = 0 ; i < s.length() ; i++) {
-        	list.add(s.substring(i, i+1));
-        }
-        
-        while(isChanged) {
-        	int cnt = list.size();
-        	int i = 1;
-        	while(i < list.size()) {
-//        	for(int i = 1 ; i < list.size() ; i++) {
-        		
-        		String b = list.get(i-1);
-        		String c = list.get(i);
-        		if(b.equals(c)) {
-        			list.remove(i);
-        			list.remove(i-1);
-        		}else {
-        			i++;
-        		}
-        		
-        	}
-        	
-        	if(list.size() == 0)
-        		return 1;
-        	if(list.size() == 1) {
-        		return 0;
-        	}
-        	if(list.size() == cnt)
-        		return 0;
-        }
 
-        return answer;
-    }
+	
 }
