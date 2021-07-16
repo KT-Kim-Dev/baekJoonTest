c860995d8d74b34ec96c797f3e8297f0a8d61716
winneris1234 <winneris1234@gmail.com>
2021-07-17 00:20:31 +0900

    프로그래머스 문제 해결 - 문자열 압축 ( 카카오 블라인드 리쿠르트먼트 )

--- example/src/programmers/example/kakao/StringCompression.java
+++ example/src/programmers/example/kakao/StringCompression.java
@@ -1,49 +1,76 @@
 package programmers.example.kakao;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.Stack;
 
 public class StringCompression {
 
 	public static void main(String[] args) {
 
-		String s = "ababcdcdababcdcd";
-		
+		String s = "xababcdcdababcdcd";
+
 		int answer = solution(s);
-		
+
 		System.out.println(answer);
-		
+
 	}
-	
+
 	public static int solution(String s) {
-        int answer = s.length();
-        
-        for(int i = 2 ; i <= s.length()/2 ; i++) {
-        	Map<String, Integer> map = new HashMap<>();
-        	
-        	for( int j = 0 ; j <= s.length() - i ; j = j+i) {
-        		String sub = s.substring(j, j+i);
-        		map.put(sub, sub.length());
-        	}
-        	
-        	
-        	Set<String> set = map.keySet();
-        	Object[] arr = set.toArray();
-        	
-        	int sum = 0;
-        	for(Object key : arr) {
-        		sum += map.get(key);
-        	}
-        	if(answer > sum)
-        		answer = sum;
-        	
-        	map.clear();
-        }
-        
-        
-        
-        return answer;
-    }
+		int answer = s.length();
+
+		for (int i = 1; i <= s.length() / 2; i++) {
+			Stack<String> stack = new Stack<>();
+
+			for (int j = 0; j <= s.length(); j = j + i) {
+				String sub = null;
+				
+				if(j+i <= s.length()) {
+					sub = s.substring(j, j + i);
+					if (stack.size() == 0)
+						stack.push(sub);
+					else {
+						String pop = stack.pop();
+						if (sub.equals(pop)) {
+							if (stack.size() != 0) {
+								String pop2 = stack.pop();
+								try {
+									int A = Integer.parseInt(pop2);
+									A++;
+									stack.push(""+A);
+									stack.push(sub);
+								} catch (NumberFormatException e) {
+									stack.push(pop2);
+									stack.push("2");
+									stack.push(sub);
+								}
+							}else {
+								stack.push("2");
+								stack.push(sub);
+							}
+						} else {
+							stack.push(pop);
+							stack.push(sub);
+						}
+					}
+				}else {
+					sub = s.substring(j, s.length());
+					stack.push(sub);
+				}
+			}
+
+			Object[] arr = stack.toArray();
+			String value = "";
+			for(Object o : arr) {
+				value += o;
+			}
+
+//			System.out.println(value);
+			if(value.equals(s))
+				continue;
+			int sum = value.length();
+			answer = answer > sum ? sum : answer;
+		}
+
+		return answer;
+	}
 
 }
