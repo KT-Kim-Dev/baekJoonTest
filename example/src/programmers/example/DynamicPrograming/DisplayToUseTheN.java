41f61604edbb2e635392e3ffd9908692109ac3ef
winneris1234 <winneris1234@gmail.com>
2021-07-18 12:44:53 +0900

    프로그래머스 DP 문제.해결 내가 못함 ..

--- example/src/programmers/example/DynamicPrograming/DisplayToUseTheN.java
+++ example/src/programmers/example/DynamicPrograming/DisplayToUseTheN.java
@@ -1,53 +1,56 @@
 package programmers.example.DynamicPrograming;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class DisplayToUseTheN {
 
 	public static void main(String[] args) {
 		int N = 5;
 		int number = 12;
-		
+
 		int answer = solution(N, number);
-		
 		System.out.println(answer);
-		
+
 	}
-	
-	public static int[] array = new int[5];
-	
+
 	public static int solution(int N, int number) {
-        int answer = 8;
-        
-        array[0] = Integer.parseInt(""+N+N);
-        
-        int A = getMinCount(N, number, N, 1);
-        int B = getMinCount(N, number, Integer.parseInt(""+N+N), 2);
-        
-        answer = A > B ? B : A;
-        
-        return answer;
-    }
-	
-	public static int getMinCount(int N, int number, int value, int cnt) {
-		int answer = 8;
-		
-		if(cnt > 8)
-			return -1;
-		
-		if(value == number)
-			return cnt;
-		
-//		int A = getMinCount(N, number, Integer.parseInt(""+value+value), cnt++);
-		int B = getMinCount(N, number, value + N, cnt++);
-		int C = getMinCount(N, number, value - N, cnt++);
-		int D = getMinCount(N, number, value * N, cnt++);
-		int E = getMinCount(N, number, value / N, cnt++);
-		
-//		answer = answer > A ? A : answer;
-		answer = answer > B ? B : answer;
-		answer = answer > C ? C : answer;
-		answer = answer > D ? D : answer;
-		answer = answer > E ? E : answer;
-		
+		int answer = -1;
+		List<Set<Long>> arr = new ArrayList<>();
+		arr.add(null);
+		arr.add(new HashSet<>());
+		arr.get(1).add(new Long(N));
+
+		for (int i = 1; i <= 8; i++) {
+			if (i >= 2) {
+				arr.add(new HashSet<>());
+
+				StringBuilder num = new StringBuilder();
+				for (int j = 0; j < i; j++) {
+					num.append(N);
+				}
+				arr.get(i).add(Long.parseLong(num.toString()));
+
+				for (int j = 1; j < i; j++) {
+					for (long k : arr.get(j)) {
+						for (long l : arr.get(i - j)) {
+							arr.get(i).add(k + l);
+							arr.get(i).add(k - l);
+							arr.get(i).add(k * l);
+							if (l != 0) {
+								arr.get(i).add(k / l);
+							}
+						}
+					}
+				}
+			}
+
+			if (arr.get(i).contains((long) number)) {
+				return i;
+			}
+		}
 		return answer;
 	}
 
