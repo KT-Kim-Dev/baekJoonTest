5e1fec83df4c71772c06738d7f9af873c5994752
winneris1234 <winneris1234@gmail.com>
2021-08-10 23:46:54 +0900

    가장 큰

--- example/src/programmers/example/sort/BiggestNumber.java
+++ example/src/programmers/example/sort/BiggestNumber.java
@@ -11,15 +11,35 @@ import java.util.Map.Entry;
 public class BiggestNumber {
 
 	public static void main(String[] args) {
-		int[][] quiz = { { 6, 10, 2 }, { 3, 30, 34, 5, 9 },
-//				{ 0, 0, 0, 0},
-				{ 151, 15 },
+		int[][] quiz = {
+				{ 6, 10, 2 },
+				{ 3, 30, 34, 5, 9 },
+				{ 10, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},	// 987654321101000
+				{ 151, 15 },	// 15151
+				{ 412, 41 },	// 41412
+				{ 303, 30 },	// 30330
+				{1, 11, 111, 1111},	
+				{10, 101},			//10110
+				{110, 10},			//11010
 //				{ 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000},
 		};
 
+		String[] answers = {
+				"6210", 
+				"9534330", 
+				"987654321101000",
+				"15151",
+				"41412",
+				"30330",
+				"1111111111",
+				"10110",
+				"11010",
+				
+		};
+		
 		for (int i = 0; i < quiz.length; i++) {
 			String answer = solution(quiz[i]);
-			System.out.println(answer);
+			System.out.println(answer + " vs " + answers[i] + " : " + answer.equals(answers[i]));
 		}
 
 	}
@@ -29,25 +49,8 @@ public class BiggestNumber {
 	public static String solution(int[] numbers) {
 		String answer = "";
 
-		answer = solution2(numbers);
+		answer = solution1(numbers);
 
-		return answer;
-	}
-	
-	public static String solution2(int[] numbers) {
-		String answer = "";
-		String[] strArray = new String[numbers.length];
-		
-		for(int i = 0 ; i < numbers.length ; i++) {
-			strArray[i] = String.valueOf(numbers[i]);
-		}
-		
-		Arrays.sort(strArray);
-		
-		for(int j = strArray.length -1 ; j >= 0 ; j--) {
-			answer += strArray[j];
-		}
-		
 		return answer;
 	}
 
@@ -59,12 +62,10 @@ public class BiggestNumber {
 		// Initialize
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		int max = 0;
-		int pnt = String.valueOf(numbers[numbers.length - 1]).length() - 1;
 		for (int i = 0; i < numbers.length; i++) {
 			if (max < numbers[i])
 				max = numbers[i];
-			long value = getPowedCnt(numbers[i], pnt);
-			map.put(i, (int) value);
+			map.put(i, numbers[i]);
 		}
 
 		if (max == 0)
@@ -74,7 +75,11 @@ public class BiggestNumber {
 
 		Collections.sort(list, new Comparator<Entry<Integer, Integer>>() {
 			public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-				return o2.getValue().compareTo(o1.getValue());
+				int A = o1.getValue();
+				int B = o2.getValue();
+				String C = String.valueOf(A);
+				String D = String.valueOf(B);
+				return (D+C).compareTo(C+D);
 			};
 		});
 
